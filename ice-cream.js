3c632bc4a1fc5d7ac170a1ddcd6771481d44381f
helenohyeah <ouyang.helen@gmail.com>
2020-08-05 18:29:12 +0000

    Simple array manipulation exercise

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+// add a flavour
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// get the first flavour
+console.log(iceCreamFlavours[0]);
+
+// get the last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+// total number of flavours
+console.log(iceCreamFlavours.length);
